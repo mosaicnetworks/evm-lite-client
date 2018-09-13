e551c69261b6bc5aaf6e709d838e14cf335b90aa
Danu Kumanan <danu3006@gmail.com>
2018-09-13 11:39:56 +0100

    Type definitions completed

--- index.js
+++
@@ -1,5 +0,0 @@
-const Controller = require('./dist/Controller');
-const utils = require('./dist/misc/utils');
-
-exports.Controller = Controller.default;
-exports.utils = utils;
--- src/evm/EVMLiteClient.ts
+++ src/evm/EVMLiteClient.ts
@@ -1,6 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http = require("http");
+import * as http from 'http'
+
 let request = (options, callback) => {
     return http.request(options, (response) => {
         // console.log(`${options.method} ${options.host}:${options.port}${options.path}`);
@@ -11,14 +10,14 @@ let request = (options, callback) => {
         response.on('end', () => {
             callback(data);
         });
-    });
+    })
 };
-class EVMBabbleClient {
-    constructor(host, port) {
-        this.host = host;
-        this.port = port;
+
+export default class EVMLiteClient {
+    public constructor(readonly host: string, readonly port: number) {
     }
-    getAccount(address) {
+
+    public getAccount(address: string): Promise<String> {
         let options = this._constructOptions('GET', `/account/${address}`);
         return new Promise((resolve, reject) => {
             let req = request(options, resolve);
@@ -26,7 +25,8 @@ class EVMBabbleClient {
             req.end();
         });
     }
-    getAccounts() {
+
+    public getAccounts(): Promise<string> {
         let options = this._constructOptions('GET', '/accounts');
         return new Promise((resolve, reject) => {
             let req = request(options, resolve);
@@ -34,25 +34,28 @@ class EVMBabbleClient {
             req.end();
         });
     }
-    call(tx) {
+
+    public call(tx: string) {
         let options = this._constructOptions('POST', '/call');
         return new Promise((resolve, reject) => {
             let req = request(options, resolve);
             req.write(tx);
             req.on('error', (err) => reject(err));
-            req.end();
+            req.end()
         });
     }
-    sendTx(tx) {
+
+    public sendTx(tx: string) {
         let options = this._constructOptions('POST', '/tx');
         return new Promise((resolve, reject) => {
             let req = request(options, resolve);
             req.write(tx);
             req.on('error', (err) => reject(err));
             req.end();
-        });
+        })
     }
-    sendRawTx(tx) {
+
+    public sendRawTx(tx: string) {
         let options = this._constructOptions('POST', '/rawtx');
         return new Promise((resolve, reject) => {
             let req = request(options, resolve);
@@ -61,21 +64,22 @@ class EVMBabbleClient {
             req.end();
         });
     }
-    getReceipt(txHash) {
+
+    public getReceipt(txHash: string) {
         let options = this._constructOptions('GET', `/tx/${txHash}`);
         return new Promise((resolve, reject) => {
             let req = request(options, resolve);
             req.on('error', (err) => reject(err));
             req.end();
-        });
+        })
     }
-    _constructOptions(method, path) {
+
+    private _constructOptions(method, path: string) {
         return {
             host: this.host,
             port: this.port,
             method: method,
             path: path
-        };
+        }
     }
 }
-exports.default = EVMBabbleClient;
